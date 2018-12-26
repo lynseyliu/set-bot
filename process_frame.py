48b4e3f7004d56f4ae0397314cc38ec5713a3d6a
Lynsey Liu <lynseyliu@gmail.com>
2018-12-25 20:43:38 -0800

    increase saturation, get number class

--- process_frame.py
+++ process_frame.py
@@ -1,10 +1,19 @@
 import numpy as np
 import cv2
 import math
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-# Read and process layout imagge
+# Read and process layout image
 layout = cv2.imread('layout.jpg')
+# Increase saturation
+layout_hsv = cv2.cvtColor(layout, cv2.COLOR_BGR2HSV).astype('float32')
+(h, s, v) = cv2.split(layout_hsv)
+s = s * 2
+s = np.clip(s, 0, 255)
+layout_hsv = cv2.merge([h, s, v])
+layout = cv2.cvtColor(layout_hsv.astype('uint8'), cv2.COLOR_HSV2BGR)
+cv2.imwrite('saturation.jpg', layout)
+# Grayscale
 layout_gray = cv2.cvtColor(layout, cv2.COLOR_BGR2GRAY)
 ret, layout_thresh = cv2.threshold(layout_gray, 127, 255, cv2.THRESH_BINARY_INV)
 layout_thresh = 255 - layout_thresh
@@ -21,10 +30,12 @@ h = 200
 rect = np.array([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]], np.float32)
 
 # Warp and classify each card
+classed_cards = set()
 for i in range(0, len(card_contours)):
     # Get contour and perimeter length, check perim for faulty contours
     card = card_contours[i]
     perim = cv2.arcLength(card, True)
+    # Ignore random contours in the layout image that aren't cards
     if perim < 100:
         continue
     # Get approx corners of card
@@ -34,7 +45,7 @@ for i in range(0, len(card_contours)):
     # Warp
     transform = cv2.getPerspectiveTransform(approx, rect)
     warp = cv2.warpPerspective(layout, transform, (w, h))
-
+    
     # It's feature extraction time!
     #  |-|   _    *  __
     #  |-|   |  *    |/'
@@ -42,17 +53,34 @@ for i in range(0, len(card_contours)):
     #  |-|   |  O o *|
     # /___\  |o___O__|
 
-    # Find shapes using contours
+    # Find shapes using contours (noisy result)
     warp_gray = cv2.cvtColor(warp, cv2.COLOR_BGR2GRAY)
     ret, warp_thresh = cv2.threshold(warp_gray, 127, 255, cv2.THRESH_BINARY_INV)
     warp_thresh = 255 - warp_thresh
     warp_thresh, shape_contours, hierarchy = cv2.findContours(warp_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Remove contour noise caused by patterns, edge
     filtered_shapes = []
-    for i, c in enumerate(shape_contours):
+    for j, c in enumerate(shape_contours):
         perim = cv2.arcLength(c, True)
-        if perim > 100 and perim < 2 * (w + h) - 100:# and hierarchy[0][i][3] == 0:
-        #if cv2.contourArea(c) > 50 and cv2.contourArea(c) < (w * h) / 2:
-            #cv2.drawContours(warp, [c], -1, (255, 0, 0), 1, 8)
+        if perim > 100 and perim < 2 * (w + h) - 100:
             filtered_shapes.append(c)
-    cv2.drawContours(warp, filtered_shapes, -1, (255, 0, 0), 1, 8)
-    cv2.imwrite('shapes' + str(i) + '.jpg', warp)
+    
+    # Remove duplicate "inner" shape contours
+    # Compare each pair of contours (will be in order outer, inner if applicable)
+    shapes_copy = filtered_shapes.copy()
+    j = 0
+    while j < len(shapes_copy) - 1:
+        shape_perim = cv2.arcLength(shapes_copy[j], True)
+        next_perim = cv2.arcLength(shapes_copy[j+1], True)
+        if next_perim < shape_perim - 10:
+            filtered_shapes.remove(shapes_copy[j+1])
+        j += 2
+    
+    # Set number class based on shape count
+    num_class = len(filtered_shapes)
+
+    # classed_cards.add({'shape': shape_class,
+    #                    'number': num_class,
+    #                    'pattern': pattern_class,
+    #                    'color': color_class})
